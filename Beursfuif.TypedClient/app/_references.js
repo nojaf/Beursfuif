abe82639938031c6565bf413dec05e9115559b7b
nojaf <florian.verdonck@outlook.com>
2014-09-27 16:54:38 +0200

    Updated Typescript version, Server builds without certificate

--- Beursfuif.TypedClient/app/_references.js
+++ Beursfuif.TypedClient/app/_references.js
@@ -1,1 +1,12 @@
+/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../scripts/typings/angularjs/angular-sanitize.d.ts" />
+/// <reference path="../scripts/typings/angularjs/angular-route.d.ts" />
+/// <reference path="../scripts/typings/jquery/jquery.d.ts" />
+/// <reference path="../scripts/typings/toastr/toastr.d.ts" />
+/// <reference path="../scripts/typings/signalr/signalr.d.ts" />
+/// <reference path="../scripts/typings/localstorage/ilocalstorageservice.d.ts" />
+/// <reference path="../models/models.ts" />
+/// <reference path="../controllers/events.ts" />
+/// <reference path="app.ts" />
+/// <reference path="../scripts/typings/moment/moment.d.ts" />
 //# sourceMappingURL=_references.js.map
